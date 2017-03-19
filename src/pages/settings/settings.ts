66628b2fcc502c83532248c531ebdccbade52455
Olga Smirnova <olgasmirnova@Olgas-MBP.localdomain>
2017-03-19 15:23:52 +1300

    settings slider syles and arrows

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -4,7 +4,6 @@ import { Storage } from '@ionic/storage';
 import { MainPage } from '../pages';
 import { IDrinks } from '../../shared/shared';
 import { DrinksService } from '../../shared/shared';
-import { NgClass } from 'angular2/common';
 
 @Component({
   selector: 'page-settings',
@@ -14,42 +13,30 @@ export class SettingsPage {
 	genderSelected: boolean = false;
 
   public drinks: IDrinks[] = [
-      {
-			family: 	'beer1',
-			name: 		'beer glass 200ml',
-			img: 		'src/assets/icon/favicon.ico',
-			contains: 	0.6
-		},
-		{	
-			family: 	'beer2',
-			name: 		'beer glass 500ml',
-			img: 		'src/assets/icon/favicon.ico',
-			contains: 	1.6
-		},
-		{	
-			family: 	'beer3',
-			name: 		'beer can',
-			img: 		'src/assets/icon/favicon.ico',
-			contains: 	1.2
-		},
-		{	
-			family: 	'beer4',
-			name: 		'beer bottle 330ml',
-			img: 		'',
-			contains: 	1.3
-		},
-		{	
-			family: 	'beer5',
-			name: 		'cider bottle 330ml',
-			img: 		'',
-			contains: 	1.3
-		},
-		{	
-			family: 	'beer6',
-			name: 		'RTD bottle 330ml',
-			img: 		'',
-			contains: 	1.6
-		}
+    	{
+				family: 	'beer',
+				name: 		'beer glass 200ml',
+				img: 			'assets/images/family/beer-family.png',
+				contains: 	0.6
+			},
+			{	
+				family: 	'wine',
+				name: 		'beer glass 500ml',
+				img: 			'assets/images/family/wine-family.png',
+				contains: 	1.6
+			},
+			{	
+				family: 	'spirits',
+				name: 		'beer can',
+				img: 			'assets/images/family/spirits-family.png',
+				contains: 	1.2
+			},
+			{	
+				family: 	'mixes',
+				name: 		'beer bottle 330ml',
+				img: 			'assets/images/family/mixes-family.png',
+				contains: 	1.3
+			}
     ];
 
 
@@ -66,6 +53,8 @@ export class SettingsPage {
     console.log('ionViewDidLoad SettingsPage');
   }
 
+
+
   // navigate to Main Page
   goToMain()
   {
@@ -73,26 +62,29 @@ export class SettingsPage {
     //this.navCtrl.setRoot(MainPage);
   }
 
+  // drinks slider arrow navigation
+  showPrevSlide()
+  {
+    let currentIndex = this.slider.getActiveIndex();
+    this.slider.slidePrev();
+  }
+  showNextSlide()
+  {
+    let currentIndex = this.slider.getActiveIndex();
+    this.slider.slideNext();
+  }
+
   // set gender
   setGender(gender)
   {
-    //this.storage.set("gender", gender);
-    this.storage.set("gender", gender).then(() => {
+      this.storage.set("gender", gender).then(() => {
       console.log('Gender has been set to ' + gender);
-			//this.genderSelected = gender;
     });
-		
-		//document.getElementById(gender + '-selected').style.display = "block";
-		 //var genderBtn = document.getElementById(gender + "-selected").classList;
-		
-
   }
 
-  // set drink family
+  // set drinks family
   defaultDrinkSelected()
   {
-    //alert(this.slider.getActiveIndex());
-
     this.storage.set("drinkFamily", this.slider.getActiveIndex());
   }
 
